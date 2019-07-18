1b3b8e78d11dfa3fa4aa53cb6b41497403608b9b
Oscar Olivares <oscarolivares625@gmail.com>
2019-07-17 23:59:00 -0400

    Configuracion inicial del servidor

--- src/config/server.js
+++ src/config/server.js
@@ -1,3 +1,15 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../app/views'));
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+module.exports = app;
